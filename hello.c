d99857a58a232fceb328ce39d0bb7649cba9b18c
anukal <anukal@iitk.ac.in>
2021-04-28 23:33:50 +0530

    23:33

--- hello.c
+++ hello.c
@@ -2,9 +2,4 @@
 void main(){
 printf("Helloworld!\n");
 printf("This must be monolithic design\n");
-micro_sendmsg("is more portable");
-}
-void micro_sendmsg(char *a)
-{
-	printf("microkernel %s\n",a);
 }
