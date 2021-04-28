9915a0284cbda79035e06a86724bd7014de6a4b9
anukal <anukal@iitk.ac.in>
2021-04-29 00:22:44 +0530

    00:22

--- hello.c
+++ hello.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 void microkernel_sendmsg(char *);
-void microkernel_getmsg(char *);
 void main(){
 printf("Helloworld!\n");
 printf("This must be monolithic design\n");
@@ -9,7 +8,4 @@ microkernel_sendmsg("is more portable");
 void microkernel_sendmsg(char *a)
 {
 	printf("microkernel %s\n",a);
-}
-void microkernel_getmsg(char *b){
-	//TODO:getmsg feature
 }
